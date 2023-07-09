296cdf41e6464c31217e7054da3dda9d898d5ca3
Juan Manuel <juan-manuel-ibarra@hotmail.com>
2023-07-09 18:47:40 -0300

    agregado boton

--- Scripts/Scripts.js
+++ Scripts/Scripts.js
@@ -6,3 +6,15 @@ console.log("No de nuevo decía");
 function sumar(a, b) {
 return a + b;
 }
+const fn = () => {
+    const miString ="soy un string"
+    let b = miString + "muy feliz"
+    console.log(b)
+}
+fn()
+
+const clickeado = () => {
+    let a ="Chanchito feliz"
+
+    console.log(a)
+}
